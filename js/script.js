4a23bc0436e08e2ff021ac5a8bbc612f71a635e7
심재형 <eowk3130@naver.com>
2019-06-12 22:55:44 +0900

    1차 . .

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log("load");
+
+
+//커서 움직이기
+$(document)
+  .mousemove(function(e) {
+    $('.cursor')
+      .eq(0)
+      .css({
+        left: e.pageX,
+        top: e.pageY
+      });
+    setTimeout(function() {
+      $('.cursor')
+        .eq(1)
+        .css({
+          left: e.pageX,
+          top: e.pageY
+        });
+    }, 100);
+  })
