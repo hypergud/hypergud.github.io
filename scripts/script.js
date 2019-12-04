3b3490a9452b6a8469b0920bb96d1229b4c57f74
심재형 <eowk3130@naver.com>
2019-12-05 06:18:47 +0900

    1205 dawn

--- scripts/script.js
+++ scripts/script.js
@@ -102,5 +102,100 @@
 
         //Run initialize.
         init();
+
+
+        // -----------------------
+
+        
+
+        $(window).scroll(function(){
+            // console.log($(window).scrollTop());
+            var scrollValue = $(window).scrollTop();
+            // var winWdith = $(window).height();
+
+            $('.about').css('text-decoration', 'none');
+          if(scrollValue  >60){
+                // var mt1 = $(".main_txt");
+                // var mt2 = $(".main_txt2");
+
+                var Size = scrollValue;
+                var Size = Size + -100;
+                var top1 = Size + (Size * 0.25);
+                var lef1 = -230 + (Size * -0.85);
+                var top2 = Size + (Size * 0.7);
+                var lef2 = 110 + (Size * 0.6);
+                var rt1 = Size / 5.8;
+                console.log(Size);
+
+                
+                // $(".main_txt").css('top', '670px');
+                    $(".main_txt").css({ WebkitTransform: 'rotate(' + -90 + 'deg)'});
+                    // $(".main_txt").css('left', '-680px');
+                    // $(".main_txt2").css('top', '1200px');
+                    // $(".main_txt2").css('left', '200px');
+
+                if(Size < 550){
+                    $(".main_txt").css('top', top1 +'px');
+                    $(".main_txt").css('left', lef1 +'px');
+                    $(".main_txt").css({ WebkitTransform: 'rotate(' + -rt1 + 'deg)'});
+                    $(".main_txt2").css('top', top2+'px');
+                    $(".main_txt2").css('left', lef2 +'px');
+
+                    $('.textgroup1').css('opacity', (Size/550));
+                $('.textgroup2').css('opacity', (Size/550));
+                }else{
+                    
+                }
+
+            }
+
+            
+            
+          else{
+              $(".main_txt").css('top', '-100px');
+              $(".main_txt").css({ WebkitTransform: 'rotate(' + 0+ 'deg)'});
+              $(".main_txt").css('left', '-250px');
+              $(".main_txt2").css('top', '-100px');
+              $(".main_txt2").css('left', '110px');
+            }
+            if(200 > scrollValue && scrollValue > 100){
+                var btsize = 100 + Size/0.95;
+                $('.index_button').css('width', btsize + 'px', 'left' , 0 + 'px');
+            }else if(scrollValue < 100){
+                $('.index_button').css('width', '10vh');
+            }
+
+            if(scrollValue >500){
+                $('.about').css('text-decoration', 'line-through');
+                if(scrollValue <700){
+                    
+                $('.gudframe').css('width', Size/1.5 + 'px', 'opacity', 1);
+                $('.gudimg').css('width', Size/1.75 + 'px', 'height', Size/1.75 + 'px', 'opacity', 1);
+                $('.obj_frame').css('width', Size/4.5 + 'px');
+                $('.obj_obj').css('width', Size/5 + 'px');
+                $('.obj_frame2').css('width', Size/4.5 + 'px');
+                $('.obj_obj2').css('width', Size/5 + 'px');
+                }
+                
+            }else{
+                $('.textgroup1').css('opacity', 0);
+                $('.textgroup2').css('opacity', 0);
+                $('.gudframe').css('width', '0px', 'height','0px', 'opacity', 0);
+                $('.gudimg').css('width', '0px', 'height','0px', 'opacity', 0);
+                $('.obj_frame').css('width', 0 + 'px', 'opacity', 0);
+                $('.obj_obj').css('width', 0 + 'px');
+                $('.obj_frame2').css('width', 0 + 'px');
+                $('.obj_obj2').css('width', 0 + 'px');
+            }
+            
+            if(scrollValue > 1100){
+                $('.about').css('text-decoration', 'none');
+                $('.contact').css('text-decoration', 'none');
+                if(scrollValue > 1350){
+                    $('.contact').css('text-decoration', 'line-through');
+                }
+            }
+          });
+        
     });
 })(jQuery);
